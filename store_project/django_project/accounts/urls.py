5a44dfb0439b3d7b598a3404eb01906d5d488aa9
Hypkeh <144148068+Hypkeh@users.noreply.github.com>
2024-08-24 00:02:52 +0600

    Delete store_project/django_project directory

--- store_project/django_project/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.UserLoginView.as_view(), name='login_view'),
-    path('logout/', views.UserLogoutView.as_view(), name='logout_view'),
-    path('register/', views.register, name='user_register'),
-    path('profile/', views.profile_view, name='user_profile'),
-]
